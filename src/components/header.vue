02efa26af84700d23069624313165a8424a36e5b
TZZ <15075318235@163.com>
2019-03-18 19:10:59 +0800

    add header

---
+++ src/components/header.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="header-box">
+    <div class="back">
+      <p class="iconfont">&#xe63b;</p>
+    </div>
+    <div class="input">
+      <p class="iconfont">&#xe61b;</p>
+      <span>请输入城市/景点/游玩主题</span>
+    </div>
+    <div class="arrow">
+      城市
+      <span class="iconfont">&#xe615;</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped lang="scss">
+@import "../assets/style/varibles.scss";
+.header-box {
+  width: 100%;
+  height: 0.86rem;
+  line-height: 0.86rem;
+  background: $bgColor;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .back {
+    width: 0.8rem;
+    height: 100%;
+    text-align: center;
+    .iconfont {
+      font-size: 0.5rem;
+      color: #fff;
+    }
+  }
+  .input {
+    flex: 1;
+    background: #fff;
+    border-radius: 0.1rem;
+    height: 0.6rem;
+    line-height: 0.5rem;
+    color: #999;
+    display: flex;
+    .iconfont {
+      width: 0.6rem;
+      height: 100%;
+      font-size: 0.5rem;
+      color: #ccc;
+    }
+    span {
+      flex: 1;
+      line-height: 0.6rem;
+      font-size: 0.29rem;
+    }
+  }
+  .arrow {
+    width: 1.5rem;
+    height: 100%;
+    font-size: 0.3rem;
+    text-align: center;
+    color: #fff;
+    .iconfont {
+      font-size: 0.25rem;
+    }
+  }
+}
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-box></header-box>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import headerBox from "../components/header";
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    headerBox
   }
-}
+};
 </script>
